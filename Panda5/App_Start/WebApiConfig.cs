f1c8dcee6d063d81e7311ad953fa415f5da2ed22
Damian Tabański <tajwan1994@wp.pl>
2017-11-28 16:57:58 +0100

    Initial commit

---
+++ Panda5/App_Start/WebApiConfig.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace Panda5
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Konfiguracja i usługi składnika Web API
+
+            // Trasy składnika Web API
+            config.MapHttpAttributeRoutes();
+
+            /*
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            */
+
+            config.Formatters.JsonFormatter.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore;
+        }
+    }
+}
---
+++ Panda5/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Panda5.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        [Route("api/products")]
+        public JArray GetAllProducts()
+        {
+            return JArray.FromObject(new NorthwindEntities().products.ToList());
+        }
+
+        [Route("api/product/{id}")]
+        public JObject GetProduct(int id)
+        {
+            product product = new NorthwindEntities().products.FirstOrDefault(x => x.productid == id);
+            return JObject.FromObject(product);
+
+            /*
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+            */
+        }
+
+    }
+}
